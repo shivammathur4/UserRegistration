33c4b9e769c98e9cb72c453af4d15b22d7c62ca8
shivammathur4 <shivamm.4aug@gmail.com>
2021-04-29 10:09:29 +0530

    validating the password rule one

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -12,6 +12,11 @@ namespace UserRegistration
         {
 
             Console.WriteLine("Welcome to User registration using unit testing!");
+            Validation regex = new Validation();
+            Console.WriteLine("Enter your Password");
+            string name = Console.ReadLine();
+            bool result = regex.ValidatePassword(name);
+            Console.WriteLine("Password: " + result);
         }
     }
 }
--- UserRegistration/Validation.cs
+++ UserRegistration/Validation.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace UserRegistration
 {
     class Validation
     {
+        public static string PASSWORD4_REGEX = "^(?=.*[0-9])(?=.*[A-Z])(?=[^!@#$%&+-.]*[!@#$%&+-.][^!@#$%&+-.]*$)[\\S]{8,}$";
+        public bool ValidatePassword(string name)
+        {
+            return Regex.IsMatch(name, PASSWORD4_REGEX);
+        }
     }
 }
