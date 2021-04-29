860634b572d481a3cebd6cb050fd8af4d27ea5ca
shivammathur4 <shivamm.4aug@gmail.com>
2021-04-29 09:53:30 +0530

    validating lastname

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -10,8 +10,12 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Welcome to User registration using unit testing!");
+                Console.WriteLine("Welcome to User registration using unit testing!");
+                Validation regex = new Validation();
+                Console.WriteLine("Enter your last name:");
+                string name = Console.ReadLine();
+                bool result = regex.ValidateName(name);
+                Console.WriteLine("Last Name: " + result);
         }
     }
 }
