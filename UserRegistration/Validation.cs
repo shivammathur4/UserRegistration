34ecbd7cb073d54807b895882a9a403abf172f1b
shivammathur4 <shivamm.4aug@gmail.com>
2021-04-29 10:05:42 +0530

    validating the password rule one

--- UserRegistration/Validation.cs
+++ UserRegistration/Validation.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace UserRegistration
 {
     class Validation
     {
+        public static string PASSWORD2_REGEX = "^(?=.*?[A-Z])[A-Za-z0-9!@#$%^&*]{8,}$";
+        public bool ValidatePassword(string name)
+        {
+            return Regex.IsMatch(name, PASSWORD2_REGEX);
+        }
     }
 }
