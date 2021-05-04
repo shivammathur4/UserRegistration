c507b8f1a08054b04092f3adc4027778da171c9b
shivammathur4 <shivamm.4aug@gmail.com>
2021-05-04 12:03:56 +0530

    Refactor Using Lambda Expression

--- UserRegistration/Validation.cs
+++ UserRegistration/Validation.cs
@@ -15,74 +15,24 @@ namespace UserRegistration
         public static string MOBILE_REGEX = "^[1-9]{1}[0-9]{1,2}[ ][1-9]{1}[0-9]{9}$";
         public static string PASSWORD4_REGEX = "^(?=.*[0-9])(?=.*[A-Z])(?=[^!@#$%&+-.]*[!@#$%&+-.][^!@#$%&+-.]*$)[\\S]{8,}$";
         public static string EMAILSAMPLE_REGEX = "^[0-9a-zA-Z]{1,}([._+-][0-9a-zA-Z]{0,})*[@][0-9a-zA-Z]{1,}.[a-zA-Z]{2,3}(.[a-zA-Z]{2,3})?$";
-
-        public bool ValidateFirstName(string name1)
+        private List<string> sampleMails = new List<string>()
         {
-
-            if (Regex.IsMatch(name1, FIRSTNAME_REGEX))
-            {
-                return true;
-            }
-            else
-            {
-                throw new CustomException(CustomException.ExceptionType.INVALID_FIRST_NAME, "Invalid first name.");
-            }
-        }
-        public bool ValidateLastName(string name2)
-        {
-            if (Regex.IsMatch(name2, LASTNAME_REGEX))
-            {
-                return true;
-            }
-            else
-            {
-                throw new CustomException(CustomException.ExceptionType.INVALID_LAST_NAME, "Invalid last name.");
-            }
-        }
-        public bool ValidateEmail(string name3)
-        {
-            if (Regex.IsMatch(name3, EMAIL_REGEX))
-            {
-                return true;
-            }
-            else
-            {
-                throw new CustomException(CustomException.ExceptionType.INVALID_EMAIL, "Invalid email address.");
-            }
-        }
-        public bool ValidateMobileNumber(string name4)
-        {
-            if (Regex.IsMatch(name4, MOBILE_REGEX))
-            {
-                return true;
-            }
-            else
-            {
-                throw new CustomException(CustomException.ExceptionType.INVALID_MOBILENO, "Invalid mobile no.");
-            }
-        }
-        public bool ValidatePassword(string name5)
-        {
-            if (Regex.IsMatch(name5, PASSWORD4_REGEX))
-            {
-                return true;
-            }
-            else
-            {
-                throw new CustomException(CustomException.ExceptionType.INVALID_PASSWORD, "Invalid password.");
-            }
-        }
-        public bool ValidateEmailSample(string name6)
-        {
-            if (Regex.IsMatch(name6, EMAILSAMPLE_REGEX))
-            {
-                return true;
-            }
-            else
-            {
-                throw new CustomException(CustomException.ExceptionType.INVALID_EMAIL, "Invalid email address.");
-            }
-        }
+            "abc@yahoo.com",
+            "abc-100@yahoo.com",
+            "abc.100@yahoo.com",
+            "abc111@abc.com",
+            "abc-100@abc.net",
+            "abc.100@abc.com.au",
+            "abc@1.com",
+            "abc@gmail.com.com",
+            "abc+100@gmail.com"
+        };
+        public bool ValidateFirstName(string name1) => (Regex.IsMatch(name1, FIRSTNAME_REGEX));
+        public bool ValidateLastName(string name2) => (Regex.IsMatch(name2, LASTNAME_REGEX));
+        public bool ValidateEmail(string name3) => (Regex.IsMatch(name3, EMAIL_REGEX));
+        public bool ValidateMobileNumber(string name4) => (Regex.IsMatch(name4, MOBILE_REGEX));
+        public bool ValidatePassword(string name5) => (Regex.IsMatch(name5, PASSWORD4_REGEX));
+        public bool ValidateEmailSample(string name6) => (Regex.IsMatch(name6, EMAILSAMPLE_REGEX));
         public void PrintResult(bool result)
         {
             if (result)
@@ -93,6 +43,10 @@ namespace UserRegistration
             {
                 Console.WriteLine("Invalid.");
             }
+        }
+        public List<string> GetList()
+        {
+            return sampleMails;
         }
     }
 }
